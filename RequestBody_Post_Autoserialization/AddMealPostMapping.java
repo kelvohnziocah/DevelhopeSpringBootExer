e22523d02f21f0e758ed45c9784acf8df801430c
kelvohnziocah <nziokakelvin02@gmail.com>
2023-10-03 22:53:50 +0300

    exercise 2

--- RequestBody_Post_Autoserialization/AddMealPostMapping.java
+++ RequestBody_Post_Autoserialization/AddMealPostMapping.java
@@ -2,10 +2,7 @@ package DevelhopeSpringBootExercises.RequestBody_Post_Autoserialization;
 
 import DevelhopeSpringBootExercises.ControllerPathVariable.Meals;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,7 +18,7 @@ public class AddMealPostMapping {
     ResponseEntity<List<Meals>> getMeal(){
         return ResponseEntity.ok(meal);
     }
-    @PostMapping("/meal")
+    @PutMapping("/meal")
     ResponseEntity<String> addMeal (@RequestBody Meals newMeal){
         meal.add(newMeal);
         return ResponseEntity.ok("Meal added successfully");
--- RequestBody_Post_Autoserialization/UpdateByNamePostMapping.java
+++ RequestBody_Post_Autoserialization/UpdateByNamePostMapping.java
@@ -1,4 +1,41 @@
 package DevelhopeSpringBootExercises.RequestBody_Post_Autoserialization;
 
+import DevelhopeSpringBootExercises.ControllerPathVariable.Meals;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class UpdateByNamePostMapping {
+    private List<Meals> meal = new ArrayList<>(Arrays.asList(
+            new Meals("ugali", "stable food", 40.00),
+            new Meals("Sweetpotatoes", "breakfast", 50.80),
+            new Meals("hotdog", "lunch meal", 150.00)
+    ));
+    @PostMapping("/meal/{name}")
+    ResponseEntity<Meals> updateByName(@PathVariable("name") String name, @RequestBody Meals updatedMeal){
+        Meals MealExists = null;
+        for (Meals meal : meal) {
+            if (meal.getName().equalsIgnoreCase(name)) {
+                MealExists = meal;
+                break;
+            }
+        }
+
+        if (MealExists != null) {
+            // Update the existing meal with the information from the updatedMeal
+            MealExists.setName(updatedMeal.getName());
+            MealExists.setDescription(updatedMeal.getDescription());
+            MealExists.setPrice(updatedMeal.getPrice());
+
+            return ResponseEntity.ok(MealExists);
+        } else {
+            // Meal not found, you can handle this case accordingly (e.g., return a not found response)
+            return null;
+        }
+    }
 }
+
